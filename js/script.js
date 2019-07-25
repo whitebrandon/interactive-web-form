32f934cf7a94081ab275955af2dce763a21ac562
Brandon White <white.brandonsean@gmail.com>
2019-07-25 13:40:19 -0400

    Added T-shirt Info functionality

--- js/script.js
+++ js/script.js
@@ -16,6 +16,49 @@
 /* ============================================
    =========== T-Shirt Info Section ===========
    ============================================ */
+   $('#design :first-child').hide(); // ← Hides 'Select Theme' from Design drop down
+   $('#color option').each(function () { // ← Hides all options in T-shirt color menu
+       $(this).hide();
+   });
+   // ↓ Prepends a new option element to the T-shirt color menu
+   $('#color').prepend($('<option>').attr('selected', true).text('Please select a T-shirt theme'));
+   // ↓ Listens for change event on Design menu and 
+   //   makes sure none of the options have the selected attribute
+   $('#design').on('change', function () {
+       $('#color option').each(function () {
+           if ($(this).attr('selected') === "selected") {
+               $(this).attr('selected', false);
+           }
+       });
+       // ↓ Shows T-shirt colors that match the JS Puns theme and hides all others
+       if ($('#design').val() === "js puns") {
+           // ↓ Sets Cornflower Blue T-shirt color as default selection
+           $('#color option[value="cornflowerblue"]').attr('selected', true);
+           $('#color option').each(function () {
+               const $index = $(this).text().indexOf('(');
+               const $theme = $(this).text().slice($index + 1, $index + 7);
+               if ($theme === "JS Pun") {
+                   $(this).show();
+               } else {
+                   $(this).hide();
+               }
+           })
+           // ↓ Shows T-shirt colors that match the I ♥ JS theme and hides all others
+       }   else if ($('#design').val() === "heart js") {
+           // ↓ Sets Tomato T-shirt color as default selection
+           $('#color option[value="tomato"]').attr('selected', true);
+           $('#color option').each(function () {
+               const $index = $(this).text().indexOf('(');
+               const $theme = $(this).text().slice($index + 1, $index + 7);
+               if ($theme === "I ♥ JS") {
+                   $(this).show();
+               } else {
+                   $(this).hide();
+               }
+           })
+
+       }
+   });
 
 /* ============================================
    ============ Activities Section ============
