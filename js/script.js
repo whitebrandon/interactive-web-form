2a1de36054275b71f180340a7e129b2182390c0f
Brandon White <white.brandonsean@gmail.com>
2019-07-25 14:58:01 -0400

    Added Activities functionality

--- js/script.js
+++ js/script.js
@@ -63,6 +63,69 @@
 /* ============================================
    ============ Activities Section ============
    ============================================ */
+   let totalCost = 0; // ← Holds cost for registered activities
+   // ↓ Creates paragraph that will hold message that displays running total
+   const $totalMsg = $('<p>');
+   // ↓ Adds paragraph to bottom of fieldset, and hides it
+   $('.activities').append($totalMsg.hide());
+   // ↓ Listens for change events that occur on inputs
+   $('.activities').on('change', 'input', function () {
+       const $currentInput = $(this); // Grabs the target of the event
+       // ↓ The next three variables collectively extract the price of the selected activity
+       const $labelText = $currentInput.parent().text();
+       const $currencyIndex = $labelText.indexOf('$');
+       const $price = parseInt($labelText.slice($currencyIndex + 1, $currencyIndex + 4), 10);
+       // ↓ If the selected activity is checked add its price to the total cost
+       //   otherwise, subtract it from the total cost
+       if ($(this).prop('checked') === true) {
+           totalCost += $price;
+       } else {
+           totalCost -= $price;
+       }
+       // ↓ If the total cost is gt 0, show the running total
+       //   otherwise, hide the running total
+       if (totalCost > 0) {
+           $totalMsg.html(`Your total is: <strong>$${totalCost}</strong>.`).show();
+       } else {
+           $totalMsg.hide();
+       }
+       // ↓ A function that returns a day of the week substring from the selected activity
+       const getDay = (string) => {
+           const $emdashIndex = string.indexOf('—');
+           const $day = string.slice($emdashIndex + 2, $emdashIndex + 12);
+           const $eodIndex = $day.indexOf(' ');
+           return $day.substring(0, $eodIndex);
+       }
+       // ↓ A function that returns a start time substring from the selected activity
+       const getTime = (string) => {
+           const $hypenIndex = string.indexOf('-');
+           return string.slice($hypenIndex - 3, $hypenIndex);
+       }
+       const $dayOf = getDay($labelText); // ← Stores day of selected activity
+       const $timeOf = getTime($labelText); // ← Stores start time of selected activity
+       $('.activities input').each(function () {
+           const $comparisonInput = $(this); // ← Grabs input for each iteration of loop
+           // ↓ Grabs text from single activity per iteration
+           const $comparisonText = $comparisonInput.parent().text(); 
+           const newDay = getDay($comparisonText); // ← Stores day of activity to be compared
+           const newTime = getTime($comparisonText); // ← Stores time of activity to be compared
+           // ↓ Resets disabled activites each time change event fires
+           if ($dayOf === newDay && 
+               $timeOf === newTime && 
+               $(this).prop('disabled') === true) {
+                   $(this).removeAttr('disabled');
+                   $(this).parent().css("text-decoration", "none");
+             // ↓ Disables activity that conflicts with day and time of selected activity
+           } else if ($dayOf === newDay &&
+                      $timeOf === newTime &&
+                      $currentInput.parent().text() !== $comparisonInput.parent().text() &&
+                      $price !== 200 &&
+                      $(this).prop('checked') !== true) {
+                          $(this).attr("disabled", true);
+                          $(this).parent().css("text-decoration", "line-through");
+                      }
+       })
+   })
 
 /* ============================================
    =========== Payment Info Section ===========
