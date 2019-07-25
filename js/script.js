301a519f175133226c2f2e3b9c81c3b918db8512
Brandon White <white.brandonsean@gmail.com>
2019-07-25 12:41:45 -0400

    Added Basic Info functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 /* ============================================
    ============ Basic Info Section ============
    ============================================ */
+   $('#name').focus(); // ← Adds focus to name input on page load
+   $('#other-title').hide(); // ← Hides other-title input on page load
+   $('#title').on('change', function () { // ← Listens for change event on Job Role drop down
+        // ↓ If client selects option "other", show Job Role text input,
+        //   otherwise, hide Job Role text input if shown
+       if ($(this).val() === "other") {
+           $('#other-title').fadeIn();
+       } else {
+           $('#other-title').fadeOut();
+       }
+   });
 
 /* ============================================
    =========== T-Shirt Info Section ===========
